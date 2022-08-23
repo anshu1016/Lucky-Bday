21f1893e17270fff122be17e83c0e19ca8ed0faa
anshu1016 <69161719+anshu1016@users.noreply.github.com>
2022-08-23 13:59:55 +0530

    Add files via upload

---
+++ luckybday.js
@@ -0,0 +1,36 @@
+const dobday = document.querySelector("#dob");
+const luckynum = document.querySelector("#l-num");
+const checkBtn = document.querySelector("#check-btn");
+const resultP = document.querySelector("#result")
+checkBtn.addEventListener("click",validateDob)
+
+function validateDob(){
+    const dob = dobday.value;
+    const sum = Amount(dob);
+    if(sum&&dob){
+        comparevalues(sum,luckynum.value);
+    }   
+    else{
+        resultP.innerText = "please enter both details.."
+    }
+}
+function Amount(dob){
+    dob = dob.replaceAll("-","");
+    let total = 0
+    for(let i=0;i<dob.length;i++)
+    {
+        total = total + Number(dob.charAt(i));
+    }
+    return total;
+}
+function comparevalues(sum,luckynum){
+    if(sum%luckynum === 0)
+    {
+        resultP.innerText = "lucky"
+    }
+    else{
+        resultP.innerText = "not lucky.."
+    }
+}
+
+//checkBtn.addEventListener('click',validateDD);
